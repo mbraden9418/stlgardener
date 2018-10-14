307276699437d593cc2125899da767ff14338c56
user <mbraden9418@yahoo.com>
2018-10-14 00:41:45 -0500

    style app component and home page

---
+++ src/app/home/monthly-tips.ts
@@ -0,0 +1,15 @@
+export const monthlyTips = [
+  // put monthly tips data here
+  {
+    month: 'January',
+    tips: [{
+      description: 'buy seeds',
+      done: false
+    }],
+
+  },
+
+  {
+month: 'February',
+  },
+];
