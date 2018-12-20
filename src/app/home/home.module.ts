7d452bcf32158f5f8f1580a04c7508351e3919bf
user <mbraden9418@yahoo.com>
2018-12-19 23:16:23 -0600

    restyle home css with material

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
 import {MatCheckboxModule} from '@angular/material';
-
+import {MatCardModule} from '@angular/material';
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule,
     MatCheckboxModule,
+    MatCardModule,
   ],
   declarations: [HomePageComponent]
 })
